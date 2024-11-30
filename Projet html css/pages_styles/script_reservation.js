40c05da1dd11b9257eb745f11739cd223bea94b9
Giopa02 <giordana.nyom@outlook.fr>
2024-12-01 00:22:20 +0100

    rendu-final
    Rendu pour l'évaluation

---
+++ Projet html css/pages_styles/script_reservation.js
@@ -0,0 +1,7 @@
+document.getElementById("valider-btn").addEventListener("click", function () {
+    // Réinitialiser les champs du formulaire
+    document.getElementById("renseignement-form").reset();
+
+    // Affiche un message pour l'utilisateur (optionnel)
+    alert("Merci ! Les informations ont été réinitialisées.");
+});
